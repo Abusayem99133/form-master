c212ad2ab716bd5e4c2312378f87508750c4105e
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-03-22 09:33:21 +0600

    new person i mean special person added to my component and my ui section

--- src/Grandpa/Dad/Uncle/Aunty/Aunty.jsx
+++ src/Grandpa/Dad/Uncle/Aunty/Aunty.jsx
@@ -1,7 +1,13 @@
+import Cousin from "../Cousin/Cousin";
+
 const Aunty = () => {
   return (
     <div>
       <h2>Aunty</h2>
+      <section>
+        <Cousin name={"Mamshad"}></Cousin>
+        <Cousin name={"Rubaiya"}></Cousin>
+      </section>
     </div>
   );
 };
